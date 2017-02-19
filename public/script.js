fd041de0b4595ba3f86bd80d677b61e4fad79baf
m. nathaniel martin <matt@angryjenkins.com>
2017-02-19 03:36:28 -0500

    added bootstrap modal on image click.

--- public/script.js
+++ public/script.js
@@ -2,9 +2,10 @@
 // To use lazy loading, set a data-lazy attribute
 // on your img tags and leave off the src
 
-
-$('.slides').slick({
-  lazyLoad: 'ondemand',
-  slidesToShow: 3,
-  slidesToScroll: 1
+$(document).ready(function(){
+  $('.slides').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
 });
